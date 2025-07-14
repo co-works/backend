a5a7d8786bb51be6f5caa04f1bdb62946c994e80
Soo186 <rlagustn9797@gmail.com>
2025-07-14 17:45:00 +0900

    initial setup

---
+++ src/main/java/com/hanjeonerp/HanjeonerpApplication.java
@@ -0,0 +1,13 @@
+package com.hanjeonerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanjeonerpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanjeonerpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanjeonerp/HanjeonerpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanjeonerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanjeonerpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
