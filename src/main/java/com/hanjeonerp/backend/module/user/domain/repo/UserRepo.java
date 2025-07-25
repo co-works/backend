11d9fff9c632e88bcbe2e36640518dc128c9c39c
Soo186 <rlagustn9797@gmail.com>
2025-07-25 17:53:31 +0900

    feat add domain user repo interface

---
+++ src/main/java/com/hanjeonerp/backend/module/user/domain/repo/UserRepo.java
@@ -0,0 +1,19 @@
+package com.hanjeonerp.backend.module.user.domain.repo;
+
+import com.hanjeonerp.backend.module.user.domain.entity.User;
+import com.hanjeonerp.backend.module.user.domain.vo.Username;
+
+import java.util.Optional;
+
+public interface UserRepo {
+    // 사용자 저장
+    User save(User user);
+    // 사용자 조회
+    Optional<User> findByUsername(Username username);
+    // 사용자 아이디 중복 체크
+    boolean existsByUsername(Username username);
+    // 사용자 ID로 조회
+    User findById(Long id);
+    // 사용자 ID로 삭제
+    void deleteById(Long id);
+}
