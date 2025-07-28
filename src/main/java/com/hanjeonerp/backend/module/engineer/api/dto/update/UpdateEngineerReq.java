4558a2a4e027fd4c96e7f43e99bfb7ab9fb7bdd2
Soo186 <rlagustn9797@gmail.com>
2025-07-28 14:40:50 +0900

    feat update req, res dto

---
+++ src/main/java/com/hanjeonerp/backend/module/engineer/api/dto/update/UpdateEngineerReq.java
@@ -0,0 +1,37 @@
+package com.hanjeonerp.backend.module.engineer.api.dto.update;
+
+import com.hanjeonerp.backend.module.engineer.domain.vo.EngineerProfile;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UpdateEngineerReq {
+
+    //엔지니어 정보
+    private String name;
+    private String phone;
+    private String email;
+    private String address;
+
+    public EngineerProfile toprofile(EngineerProfile existingProfile) {
+        String name = existingProfile.getName();
+        if (this.name !=null)name =this.name;
+
+        String phone = existingProfile.getPhone();
+        if (this.phone != null) phone = this.phone;
+
+        String email = existingProfile.getEmail();
+        if (this.email != null) email = this.email;
+
+        String address = existingProfile.getAddress();
+        if (this.address != null) address = this.address;
+
+        return new EngineerProfile(
+                name,
+                phone,
+                email,
+                address
+        );
+    }
+}
