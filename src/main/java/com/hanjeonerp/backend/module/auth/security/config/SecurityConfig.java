d81d28356842915295609f0238e9dec6a7260ccb
Soo186 <rlagustn9797@gmail.com>
2025-09-03 10:09:02 +0900

    feat : front domain Cors 추가

--- src/main/java/com/hanjeonerp/backend/module/auth/security/config/SecurityConfig.java
+++ src/main/java/com/hanjeonerp/backend/module/auth/security/config/SecurityConfig.java
@@ -43,7 +43,8 @@ public class SecurityConfig {
     private static final List<String> ALLOWED_ORIGINS = List.of(
             "http://localhost:3000",
             "http://127.0.0.1:3000",
-            "http://localhost:5173"
+            "http://localhost:5173",
+            "https://kepco-management.vercel.app"
     );
 
     /*
