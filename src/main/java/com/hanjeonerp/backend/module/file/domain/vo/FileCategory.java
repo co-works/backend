ef37c8cc9d428444d3785abe9e604657c8ffc07e
leeroot <dev.rootlee@gmail.com>
2025-10-23 21:30:25 +0900

    타당성 검토 보고서 추가

--- src/main/java/com/hanjeonerp/backend/module/file/domain/vo/FileCategory.java
+++ src/main/java/com/hanjeonerp/backend/module/file/domain/vo/FileCategory.java
@@ -12,6 +12,7 @@ public enum FileCategory {
     GOMETA_EXCEL("입주사별 전력사용량 자료"),     // 조건부 필수 - 등록 시 제출
 
     INSPECTION_REPORT("실사 검토 보고서"),
+    FEASIBILITY_REPORT("타당성 검토 보고서"),  // 25.10.23 타당성검토 서류 추가
     CONTRACT("계약서"),
     SAVINGS_PROOF("전기요금 절감 확인서"),
     INSURANCE("보증 보험 증권"),
